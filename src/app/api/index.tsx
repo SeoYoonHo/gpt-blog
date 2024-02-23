7951e29377effdec05c4445321e03b5974f1118c
seoyoonho <gogoy2643@naver.com>
2024-02-23 23:56:56 +0900

    일단 검색기능 넣었음. 오류많으므로 잡아야함

--- src/app/api/index.tsx
+++ src/app/api/index.tsx
@@ -1,12 +1,20 @@
 import axios from 'axios';
 
-const apiClient = axios.create({
-  baseURL: 'http://localhost:8080',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+const baseURL = 'http://localhost:8080';
+
+const getAllPostURL = '/api/v1/post/searchList';
+const createGptPostURL = '/api/v1/openai/question';
 
 export const getAllPost = async () => {
-  return await apiClient.get('/api/v1/post/searchList');
+  return await axios.get(baseURL + getAllPostURL, {
+    params: {
+      size: 20,
+    }
+  });
+};
+
+export const createGptPost = async () => {
+  return await axios.post(baseURL + createGptPostURL, {
+    contents: '스프링 jpa에서 엔터티의 필드 타입으로 String[] 을 선언할 수 없어? 대체방법의 예제코드도 알려줘',
+  });
 };
--- src/components/filter/search-bar.tsx
+++ src/components/filter/search-bar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
 
 import { BsSearch } from 'react-icons/bs';
 import { IoMdClose } from 'react-icons/io';
@@ -9,9 +9,11 @@ import { useRecoilState } from 'recoil';
 import useFocus from '@/hooks/use-focus';
 import useHover from '@/hooks/use-hover';
 import { queryState } from '@/states/query';
+import {createGptPost} from '../../app/api/index'
 
 export default function SearchBar() {
   const [query, setQuery] = useRecoilState(queryState);
+  const [tempQry, setTempQry] = useState('');
   const { ref: hoverRef, isHovering } = useHover<HTMLDivElement>();
   const { ref: focusRef, isFocusing } = useFocus<HTMLInputElement>();
 
@@ -20,8 +22,22 @@ export default function SearchBar() {
   };
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setQuery(e.target.value);
+    // setQuery(e.target.value);
+    setTempQry(e.target.value);
   };
+
+  const handleEnterKey = async (e) => {
+    try{
+      if (e.key === 'Enter') {
+        console.log('엔터 키가 눌렸습니다!');
+        const response = await createGptPost();
+        console.log(response);
+        setQuery(tempQry);
+      }
+    } catch (error){
+      console.log('error dddd', error);
+    }
+  }
 
   return (
     <div
@@ -40,7 +56,8 @@ export default function SearchBar() {
         type="text"
         placeholder="Search posts"
         onChange={handleInputChange}
-        value={query}
+        onKeyDown={handleEnterKey}
+        value={tempQry}
         className={`w-full rounded-full border-[2px] border-gray-300 py-4 pl-14 pr-12 text-xl font-medium hover:border-red-500 focus:border-red-500 focus:bg-customGray-base focus:outline-none dark:border-gray-400 dark:bg-customGray-dark dark:focus:bg-customGray-light ${
           isHovering && 'border-red-500'
         }`}
